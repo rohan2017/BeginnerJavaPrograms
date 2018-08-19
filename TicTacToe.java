cd6c72f979969574463057dbd523408cde7477d1
rohan2017 <rohanbandaru1@gmail.com>
2018-08-19 13:00:19 -0400

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+package simpleGames;
+
+public class TicTacToe {
+	public static void main(String args[]){
+		int [][] gameboard = {{0,0,0},{0,0,0},{0,0,0}};
+		boolean win = false;
+		int turn  = 0;
+		double[] topright = {1,1,1,1,1,1,1,1,1};
+		double[] topleft = {1,1,1,1,1,1,1,1,1};
+		double[] top = {1,1,1,1,1,1,1,1,1};
+		double[] left = {1,1,1,1,1,1,1,1,1};
+		double[] right = {1,1,1,1,1,1,1,1,1};
+		double[] middle = {1,1,1,1,1,1,1,1,1};
+		double[] botright = {1,1,1,1,1,1,1,1,1};
+		double[] botleft = {1,1,1,1,1,1,1,1,1};
+		double[] bottom = {1,1,1,1,1,1,1,1,1};
+		while(win){
+			if(turn == 1){
+				
+			}
+		}
+	}
+}
